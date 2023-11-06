1c68405576633138ce1918f3cc85b78ff9f28d88
larahynkim <lara.kimcf@gmail.com>
2023-11-07 02:39:26 +0900

    fix errors in workflow

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     version='0.1.1',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    python_requires='>=3.10, <3.12',
+    install_requires=[
+        'pytest>=6.2',  
+    ],
 )
